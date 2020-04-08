17ee3d08e5ba3c8a0633f0793c0ed4bb45d1facb
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-04-08 03:59:32 -0500

    Update script.js
    refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,47 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-
-var createTaskHandler = function(event) {
+    // ****** Form Handler
+var taskFormhandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+        // ****** package update as an object
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
     
+        // ****** Send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+} 
+
+    // ****** Create task Element
+var createTaskEl = function(taskDataObj) {
+ 
         // ****** create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-//    listItemEl.textContent = taskNameInput;
-        // ****** create div to hold task info and add to list item
+
+         // ****** create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
 
         // ****** give it a class name
     taskInfoEl.className = "task-info";
     
         // ****** add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
 
         // ****** add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-//    console.log(event);
-} 
+}
 
-
-formEl.addEventListener("submit", createTaskHandler); {
+    // ****** Event Listener
+formEl.addEventListener("submit", taskFormhandler); {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = taskNameInput;
+}
